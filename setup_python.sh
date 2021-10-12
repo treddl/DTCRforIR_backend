5b41538eb3665b9603dabf40205bb022147dd267
treddl <73252042+treddl@users.noreply.github.com>
2021-10-12 11:51:51 +0200

    updated pip3 install method

--- setup_python.sh
+++ setup_python.sh
@@ -1,6 +1,4 @@
-apt-get install -y screen &&
-apt-get install -y python3 &&
-sudo apt-get -y install python3-distutils &&
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py &&
-python3 get-pip.py &&
+sudo apt install -y screen &&
+sudo apt install -y python3 &&
+sudo apt install -y python3-pip &&
 pip3 install flask flask-cors 
