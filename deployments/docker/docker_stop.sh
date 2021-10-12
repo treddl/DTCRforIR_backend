0081e2adbe25862d02e37bdef15b4c8174619b8b
treddl <73252042+treddl@users.noreply.github.com>
2021-10-12 12:17:25 +0200

    add comments and bash script

--- deployments/docker/docker_stop.sh
+++ deployments/docker/docker_stop.sh
@@ -1,6 +1,13 @@
+# preparation to stop containers gracefully
 cd ../../src
+## reverses the dsiem directives from .json to .txt
 bash deactivate_directives.sh
+## quits all running screen sessions in the Docker container 'digital_twin'
+sudo docker exec -it digital_twin bash quit_screen.sh
+## quits the screen 'frontend' in the baselayer VM
 screen -X -S frontend quit
+
+# stop containers
 cd ../deployments/docker
 sudo docker-compose stop 
 sudo docker container rm elasticsearch 
