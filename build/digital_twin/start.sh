9684c85d8063adb5de79993ffa3c35d80b8629e1
treddl <73252042+treddl@users.noreply.github.com>
2021-10-12 15:30:35 +0200

    added comments

--- build/digital_twin/start.sh
+++ build/digital_twin/start.sh
@@ -3,6 +3,7 @@
 cd src
 ls
 service openvswitch-switch start
+echo 'running command "mn -c"'
 mn -c
 rm ./logs/plc1.log
 rm ./logs/plc2.log
@@ -10,6 +11,11 @@ rm ./logs/plc3.log
 rm ./logs/tcpdump.log
 rm ./logs/firewall.log
 
+# initiates the sqlite database for the physical filling process
+echo 'running src/init.py'
 python init.py
-screen -dmSL main python run.py
+
+# runs the main python script run.py
+echo 'running src/run.py in screen "main_run"'
+screen -dmSL main_run python run.py
 tail -f /dev/null
