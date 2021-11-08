a5db01b19d96e9bb872e8a82cc7244dc7437d830
lenaglas <lena.glas@web.de>
2021-11-08 09:59:32 +0000

    removed legacy files

--- src/.bashrc
+++
@@ -1,9 +0,0 @@
-alias screenls='screen -ls'
-alias screenr='screen -r'
-screenq () {screen -X -S  quit}
-alias screenls='screen -ls'
-alias screenr='screen -r'
-screenq () {screen -X -S  quit}
-alias screenls='screen -ls'
-alias screenr='screen -r'
-screenq () {screen -X -S  quit}
--- src/nids_hmi.py
+++ src/nids_hmi.py
@@ -1,4 +1,4 @@
-from utils import PLC1_ADDR, PLC2_ADDR, PLC3_ADDR, HMI_ADDR
+from utils import PLC1_ADDR, PLC2_ADDR, PLC3_ADDR, HMI_ADDR, ATTCKR_ADDR
 from scapy.all import *
 import logging
 import time
@@ -10,22 +10,19 @@ Implements a scpay-based network intrusion detection system (NIDS).
 
 The NIDS function is achieved by performing ARP scans on the network.
 
-The ARP scan reveals if there's an unknown host in the network.
+
 """
 
 
 def arp_scan():
-    # list of known hosts, i.e., all hosts except for the Attacker
-    known_servers=[PLC1_ADDR, PLC2_ADDR, PLC3_ADDR, HMI_ADDR]
+    # sets log format 
+    logging.basicConfig(filename='logs/nids_hmi.log',format='%(levelname)s %(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
     
+    # list of known hosts
+    known_hosts=[PLC1_ADDR, PLC2_ADDR, PLC3_ADDR, HMI_ADDR, ATTCKR_ADDR]
     
-    # NB: this script assumes there's an attacker on host 10.0.0.4
-    
-    # IP Address for the destination
-    target_ip = "10.0.0.4/24"
-    
-    # create ARP packet
-    arp = ARP(pdst=target_ip)
+    # create ARP frame
+    arp = ARP(pdst="10.0.0.4/24")
     
     # create the Ether broadcast packet
     # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
@@ -34,24 +31,26 @@ def arp_scan():
     # stack them
     packet = ether/arp
 
+    # performing ARP scan in broadcast
     result = srp(packet, timeout=3, verbose=0)[0]
 
     # a list of clients, we will fill this in the upcoming loop
     clients = []
-
-    # sets log format 
-    logging.basicConfig(filename='logs/nids_hmi.log',format='%(levelname)s %(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
     
     for sent, received in result:
         clients.append(received.psrc)
      
     for client in clients:
-        if client not in known_servers:
-            print("possible attacker: ",client)
-            log=HMI_ADDR+" %(ATTACKER_ADDR)s FIREWALL-WARNING: Unkown IP address in network: %(ATTACKER_ADDR)s" %{"ATTACKER_ADDR": client}
-            print(log)
-            logging.warning(log)
-            
+        log="%(HMI_ADDR)s %(HMI_ADDR)s TESTING-SIEM-ALARM nids_hmi:: WARNING: successfully reached host: %(client)s" %{"HMI_ADDR": HMI_ADDR, "client": client}
+        print(log)
+        logging.warning(log)
+        time.sleep(3)
+
+        
+arp_scan()
+
+"""
 while True:
     arp_scan()
     time.sleep(30)
+"""
