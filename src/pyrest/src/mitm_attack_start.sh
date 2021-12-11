5b003833f924f9260f28aeff7971c15a74fa306c
lenaglas <lena.glas@web.de>
2021-12-11 14:58:54 +0000

    minor fix

--- src/pyrest/src/mitm_attack_start.sh
+++ src/pyrest/src/mitm_attack_start.sh
@@ -7,5 +7,5 @@ docker exec digital_twin screen -S attacker_mitm -X stuff "bash attack/mitm_atta
 
 # notes
 ## docker exec digital_twin: runs the arguments that follow in inside the digtial_twin container
-## screen -S attacker -X: runs the following command in the detached screen session named 'attacker'
+## screen -S attacker_mitm -X: runs the following command in the detached screen session named 'attacker'
 ## stuff "bash attack/mitm_attack.sh^M": argument/command that is run inside the screen session named 'attacker', '^M' corrensponds to hitting enter on the given input 
