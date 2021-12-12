c98463fb468aa393d7731bed2df1b4c5c9e415b7
lenaglas <lena.glas@web.de>
2021-12-12 21:00:02 +0000

    restore srcript

---
+++ src/pyrest/src/mitm_attack_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# starts the MitM attack via ARP spoofing
+echo 'run mitm_attack_start.sh'
+
+
+docker exec digital_twin screen -S attacker_mitm -X stuff "bash attack/mitm_attack.sh^M"
+
+# notes
+## docker exec digital_twin: runs the arguments that follow in inside the digtial_twin container
+## screen -S attacker_mitm -X: runs the following command in the detached screen session named 'attacker'
+## stuff "bash attack/mitm_attack.sh^M": argument/command that is run inside the screen session named 'attacker', '^M' corrensponds to hitting enter on the given input 
